1dd7907aa554587c7390645b48345e7d7d29dfd8
andreibindasov <>
2020-10-15 16:01:12 -0600

    searchbox implemented fuzzy tried

--- ui/src/screens/pokemon/Pokemon.tsx
+++ ui/src/screens/pokemon/Pokemon.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import { RouteComponentProps, Link } from '@reach/router'
 import { useQuery, gql } from '@apollo/client'
 import { Container as NesContainer } from 'nes-react'
+
+import { useFuzzy } from 'react-use-fuzzy'
 
 const Container = styled(NesContainer)`
   && {
@@ -69,10 +71,13 @@ const POKEMON_MANY = gql`
 const Pokemon: React.FC<RouteComponentProps & { clickLink: Function }> = ({
   clickLink,
 }) => {
+  
   const { loading, error, data } = useQuery(POKEMON_MANY)
   const pokemonList:
     | Array<{ id: string; name: string; img: string; num: string }>
     | undefined = data?.pokemonMany
+
+  const [searchState, setSearchState] = useState<{id: string; name: string; img: string; num: string}[]>([]);
 
   if (loading) {
     return <p>Loading...</p>
@@ -81,12 +86,27 @@ const Pokemon: React.FC<RouteComponentProps & { clickLink: Function }> = ({
     return <p>Error!</p>
   }
 
+  // const filteredPokemons = pokemonList.filter(pokemon=>
+  //   // pokemon.name.toLowerCase().includes(searchState.toLowerCase())
+  //   fuzzysearch(searchState.toLowerCase(), pokemon.name.toLowerCase())
+  // );
+
+  const {result, keyword='', search} = useFuzzy<{id: string; name: string; img: string; num: string}>(searchState, {
+    keys: ['name'],
+  })
+
   return (
     <div>
       <SearchBox>
           <h3>Search Box</h3>
           
-            <Input></Input>
+      <Input 
+          value={keyword} 
+          onChange={(e)=>{
+            // setSearchState(e.target.value)
+            search(e.target.value)
+          }}
+      ></Input>
          
       </SearchBox>
       <Filters>
@@ -100,18 +120,20 @@ const Pokemon: React.FC<RouteComponentProps & { clickLink: Function }> = ({
       <Container rounded>
         
         <List>
-          {pokemonList.map(pokemon => (
-            <Link to={pokemon.id} onMouseDown={clickLink as any}>
-              <ListItem>
-                <img src={pokemon.img} />
-                {pokemon.name} - {pokemon.num}
-              </ListItem>
-            </Link>
+          {result.map(pokemon => (
+              <Link to={pokemon.id} onMouseDown={clickLink as any}>
+                <ListItem>
+                  <img src={pokemon.img} />
+                  {pokemon.name} - {pokemon.num}
+                </ListItem>
+              </Link>
           ))}
         </List>
       </Container>
     </div>
   )
 }
+
+
 
 export default Pokemon
